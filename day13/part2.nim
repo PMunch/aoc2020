cd4d7a1a08880e4100bfcee1baca8ac09ab75ff8
Peter Munch-Ellingsen <peterme@peterme.net>
2020-12-13 20:43:03 +0100

    Cleanup part2

--- day13/part2.nim
+++ day13/part2.nim
@@ -10,23 +10,14 @@ for i, bus in buses:
   if bus != "x":
     deltaBuses.add (i, parseInt(bus))
 
-# Find where the two first buses first match
-var start = 0
-while true:
-  if (start + deltaBuses[1].index) mod deltaBuses[0].bus == 0 and
-     (start + deltaBuses[1].index) mod deltaBuses[1].bus == 0:
-    break
-  inc start
-
-# Start looking for the third bus, at bus 1 * bus 2 increments. This is the
-# increment at which these two buses will always be in the same position
 var
-  currentBus = 2
-  stepDistance = deltaBuses[0].bus * deltaBuses[1].bus
+  start = 0
+  currentBus = 1
+  stepDistance = deltaBuses[0].bus
 
-# Loop through all the buses, increasing the step distance by the current bus
-# every time we find an matching position. Only start looping from the current
-# match.
+# Loop through all the buses, increasing the step distance by multiplying it
+# with the current bus every time we find a matching position. Only start
+# looping from the current match.
 while currentBus < deltaBuses.len:
   for i in countup(start, int.high, stepDistance):
     if (i + deltaBuses[currentBus].index) mod deltaBuses[currentBus].bus == 0:
